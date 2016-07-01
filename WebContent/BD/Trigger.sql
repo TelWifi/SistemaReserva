b62dafe7013b61183c58629d17f855c9767705f2
EmilioPantaleon <emilio.pantaleon90@gmail.com>
2016-06-30 22:45:52 -0500

    Update Trigger.sql

--- WebContent/BD/Trigger.sql
+++ WebContent/BD/Trigger.sql
@@ -3,8 +3,7 @@ create trigger check_precio before insert on preciohora
 for each row
 begin
   if new.precio < 0.0 then
-    signal sqlstate '45000';
-      set message_text = 'Precio tiene que se mayor a 0';
+    signal sqlstate '45000'set message_text = 'Error';
   end if;
 end;
 //
