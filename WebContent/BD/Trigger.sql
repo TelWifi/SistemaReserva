01d74332d890d54b2ca569e064ec6edd69affb74
EmilioPantaleon <emilio.pantaleon90@gmail.com>
2016-07-01 11:04:08 -0500

    Update Trigger.sql

--- WebContent/BD/Trigger.sql
+++ WebContent/BD/Trigger.sql
@@ -1,6 +1,29 @@
-create trigger check_precio before insert on preciohora for each row
-begin
-  if new.precio < 0.0 then
-    signal sqlstate '45000'set message_text = 'Error precio';
-  end if;
-end;
+DROP TRIGGER IF EXISTS check_ins_preciohora;
+DELIMITER $$
+CREATE TRIGGER check_ins_preciohora BEFORE INSERT ON preciohora FOR EACH ROW 
+BEGIN
+	if new.diaSemana not IN ('L','M','X','J','V','S','D') THEN
+    	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error dia semana';
+	END IF;
+	if new.precio < 0.0 then
+		SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error precio mayor a 0';
+	end if;
+	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
+	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado';
+	end if;
+END;
+
+DROP TRIGGER IF EXISTS check_upd_preciohora;
+DELIMITER $$
+CREATE TRIGGER check_upd_preciohora BEFORE UPDATE ON preciohora FOR EACH ROW 
+BEGIN
+	if new.diaSemana not IN ('L','M','X','J','V','S','D') THEN
+    	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error dia semana';
+	END IF;
+	if new.precio < 0.0 then
+		SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error precio mayor a 0';
+	end if;
+	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
+	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado';
+	end if;
+END;
