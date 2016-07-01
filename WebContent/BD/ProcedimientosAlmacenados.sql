6cbb167bc28f55422edffbed161d179fb999fe33
EmilioPantaleon <emilio.pantaleon90@gmail.com>
2016-07-01 16:23:55 -0500

    Update ProcedimientosAlmacenados.sql

--- WebContent/BD/ProcedimientosAlmacenados.sql
+++ WebContent/BD/ProcedimientosAlmacenados.sql
@@ -14,3 +14,19 @@ BEGIN
   SELECT * FROM Usuario WHERE email=_email && password =_password
 END;
 //
+
+DELIMITER $$
+CREATE PROCEDURE paAsignarOperador (nomSede varchar(50), dniOpe char(8), fechaIni time)
+BEGIN
+	DECLARE idSe int;
+    DECLARE idOpe int;
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    BEGIN
+    	ROLLBACK;
+    END;
+    SET idSe = (SELECT * FROM sede WHERE nombre = nomSede);
+    SET idOpe = (SELECT * FROM operador WHERE Operador_idUsuario = (SELECT idUsuario FROM usuario WHERE DNI = dniOpe));
+    START TRANSACTION;
+    INSERT INTO dirige (inicio, Dirige_idSede, Dirige_idOperador) VALUES (fechaIni, idSe, idOpe);
+COMMIT;
+END;
